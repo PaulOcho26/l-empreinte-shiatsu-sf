a0947f19526735037c85ba344e925f3f3f1d272d
PaulOcho26 <paul.bourlier2026@gmail.com>
2026-05-24 11:56:36 +0200

    Correction de l'entité Service

--- src/Entity/Service.php
+++ src/Entity/Service.php
@@ -3,6 +3,7 @@
 namespace App\Entity;
 
 use App\Repository\ServiceRepository;
+use Doctrine\DBAL\Types\Types;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ServiceRepository::class)]
@@ -14,36 +15,81 @@ class Service
     private ?int $id = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $string = null;
+    private ?string $name = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column]
+    private ?int $duration = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
 
     #[ORM\Column(length: 255)]
-    private ?string $no = null;
+    private ?string $slug = null;
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getString(): ?string
+    public function getName(): ?string
     {
-        return $this->string;
+        return $this->name;
     }
 
-    public function setString(string $string): static
+    public function setName(string $name): static
     {
-        $this->string = $string;
+        $this->name = $name;
 
         return $this;
     }
 
-    public function getNo(): ?string
+    public function getDescription(): ?string
     {
-        return $this->no;
+        return $this->description;
     }
 
-    public function setNo(string $no): static
+    public function setDescription(string $description): static
     {
-        $this->no = $no;
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDuration(): ?int
+    {
+        return $this->duration;
+    }
+
+    public function setDuration(int $duration): static
+    {
+        $this->duration = $duration;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): static
+    {
+        $this->slug = $slug;
 
         return $this;
     }
