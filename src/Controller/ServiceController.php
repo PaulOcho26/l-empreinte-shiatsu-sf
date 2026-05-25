a1e4249c9ca11b7b2adab61cc623f8a3f7d01784
PaulOcho26 <paul.bourlier2026@gmail.com>
2026-05-25 16:00:44 +0200

    Ajustement responsive Titre, Palette Zen Bronze et police Cormorant

--- src/Controller/ServiceController.php
+++ src/Controller/ServiceController.php
@@ -2,17 +2,34 @@
 
 namespace App\Controller;
 
+use App\Repository\ServiceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class ServiceController extends AbstractController
 {
-#[Route('/services', name: 'app_service')]
-    public function index(\App\Repository\ServiceRepository $serviceRepository): \Symfony\Component\HttpFoundation\Response
+    #[Route('/services', name: 'app_service')]
+    public function index(ServiceRepository $serviceRepository): Response
     {
         return $this->render('service/index.html.twig', [
             'services' => $serviceRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/services/{slug}', name: 'app_service_show')]
+    public function show(string $slug, ServiceRepository $serviceRepository): Response
+    {
+        // On cherche le service en base de données par son "slug"
+        $service = $serviceRepository->findOneBy(['slug' => $slug]);
+
+        // Si le service n'est pas trouvé (mauvaise URL), on génère une page 404
+        if (!$service) {
+            throw $this->createNotFoundException('Désolé, ce soin n\'existe pas encore.');
+        }
+
+        return $this->render('service/show.html.twig', [
+            'service' => $service,
         ]);
     }
 }
