94bd76f029690299cc1d88d5ea5c00d4a3044a29
PaulOcho26 <paul.bourlier2026@gmail.com>
2026-07-21 16:27:28 +0200

    Fix: Sécurisation ROLE_ADMIN, CRUD Soins/Lexique et correction Login UI

--- src/Controller/LexiconController.php
+++ src/Controller/LexiconController.php
@@ -2,21 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Lexicon;
+use App\Form\LexiconType;
 use App\Repository\LexiconRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/lexicon')]
 final class LexiconController extends AbstractController
 {
-    #[Route('/lexique', name: 'app_lexicon_index')]
+    #[Route(name: 'app_lexicon_index', methods: ['GET'])]
     public function index(LexiconRepository $lexiconRepository): Response
     {
-        // On récupère tous les termes triés par ordre alphabétique
-        $terms = $lexiconRepository->findBy([], ['term' => 'ASC']);
-
         return $this->render('lexicon/index.html.twig', [
-            'terms' => $terms,
+            'lexicons' => $lexiconRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_lexicon_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $lexicon = new Lexicon();
+        $form = $this->createForm(LexiconType::class, $lexicon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($lexicon);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_lexicon_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('lexicon/new.html.twig', [
+            'lexicon' => $lexicon,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_lexicon_show', methods: ['GET'])]
+    public function show(Lexicon $lexicon): Response
+    {
+        return $this->render('lexicon/show.html.twig', [
+            'lexicon' => $lexicon,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_lexicon_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Lexicon $lexicon, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(LexiconType::class, $lexicon);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_lexicon_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('lexicon/edit.html.twig', [
+            'lexicon' => $lexicon,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_lexicon_delete', methods: ['POST'])]
+    public function delete(Request $request, Lexicon $lexicon, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$lexicon->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($lexicon);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_lexicon_index', [], Response::HTTP_SEE_OTHER);
     }
 }
--- src/Controller/TreatmentController.php
+++ src/Controller/TreatmentController.php
@@ -2,21 +2,80 @@
 
 namespace App\Controller;
 
+use App\Entity\Treatment;
+use App\Form\TreatmentType;
 use App\Repository\TreatmentRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
+#[Route('/treatment')]
 final class TreatmentController extends AbstractController
 {
-    #[Route('/soins', name: 'app_treatment_index')] // On change /treatment par /soins pour l'élégance
+    #[Route(name: 'app_treatment_index', methods: ['GET'])]
     public function index(TreatmentRepository $treatmentRepository): Response
     {
-        // On récupère uniquement les soins actifs, classés par prix
-        $soins = $treatmentRepository->findBy(['isActive' => true], ['price' => 'ASC']);
-
         return $this->render('treatment/index.html.twig', [
-            'soins' => $soins,
+            'treatments' => $treatmentRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_treatment_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EntityManagerInterface $entityManager): Response
+    {
+        $treatment = new Treatment();
+        $form = $this->createForm(TreatmentType::class, $treatment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($treatment);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_treatment_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('treatment/new.html.twig', [
+            'treatment' => $treatment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_treatment_show', methods: ['GET'])]
+    public function show(Treatment $treatment): Response
+    {
+        return $this->render('treatment/show.html.twig', [
+            'treatment' => $treatment,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_treatment_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Treatment $treatment, EntityManagerInterface $entityManager): Response
+    {
+        $form = $this->createForm(TreatmentType::class, $treatment);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_treatment_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->render('treatment/edit.html.twig', [
+            'treatment' => $treatment,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_treatment_delete', methods: ['POST'])]
+    public function delete(Request $request, Treatment $treatment, EntityManagerInterface $entityManager): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$treatment->getId(), $request->getPayload()->getString('_token'))) {
+            $entityManager->remove($treatment);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('app_treatment_index', [], Response::HTTP_SEE_OTHER);
     }
 }
