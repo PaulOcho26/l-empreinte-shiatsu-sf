fbd898f74b92ca403cbd5283e73216b6a6fb16b9
PaulOcho26 <paul.bourlier2026@gmail.com>
2026-07-23 16:44:28 +0200

    Sprint J-14 FIN: Ajout de Charles en Fixtures et calage typographique final

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -14,8 +14,9 @@ class UserFixtures extends Fixture
 
     public function __construct(private UserPasswordHasherInterface $hasher) {}
 
-    public function load(ObjectManager $manager): void
+public function load(ObjectManager $manager): void
     {
+        // 1. Création de Sandrine (L'Admin)
         $admin = new User();
         $admin->setEmail('sandrine@l-empreinte.fr');
         $admin->setFirstName('Sandrine');
@@ -25,9 +26,22 @@ class UserFixtures extends Fixture
         $admin->setPassword($this->hasher->hashPassword($admin, 'Azerty123!'));
 
         $manager->persist($admin);
+
+        // 2. Création de Charles (Le Patient permanent)
+        $charles = new User();
+        $charles->setEmail('charles.debord@live.fr');
+        $charles->setFirstName('Charles');
+        $charles->setLastName('Debord');
+        $charles->setRoles(['ROLE_USER']);
+        $charles->setWallet('0.00');
+        $charles->setPassword($this->hasher->hashPassword($charles, 'Azerty123!'));
+
+        $manager->persist($charles);
+
+        // 3. ENREGISTREMENT FINAL (On flush une seule fois pour tout le monde)
         $manager->flush();
 
-        // On enregistre la référence pour les autres fichiers
+        // 4. RÉFÉRENCE (On garde la référence de Sandrine pour le Lexique/Articles)
         $this->addReference(self::ADMIN_USER_REFERENCE, $admin);
     }
 }
